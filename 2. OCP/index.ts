df61cc48c6126bb09ffb06d440a3b50f85957bf7
Angular CLI <angular-cli@angular.io>
2018-03-07 13:58:38 -0500

    chore: initial commit from @angular/cli

---
+++ 2. OCP/index.ts
@@ -0,0 +1,19 @@
+// class CreditCard {
+//     private nombre : string;
+
+
+//     getNombreTarjeta(){
+//         return  "Tarjeta generica";
+//     }
+// }
+
+
+// class TestOCP {
+
+//     constructor() {
+//         let tarjeta : CreditCard = new CreditCard();
+//         console.log(tarjeta.getNombreTarjeta());
+//     }
+
+// }
+
